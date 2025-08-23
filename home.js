699a7bf5b2430d04c9ecc20c88e8ef31847b0ce5
rakibhassannayem <rakibhassannayem@gmail.com>
2025-08-23 19:31:08 +0600

    add money functionality done

---
+++ home.js
@@ -0,0 +1,31 @@
+const validPin = 1234
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const bank = document.getElementById("bank").value;
+    const accountNumber = 
+      document.getElementById("account-number").value
+    ;
+    const amount = parseInt(document.getElementById("add-amount").value);
+    const pin = parseInt(document.getElementById("add-pin").value);
+
+    const availableBalance = parseInt(
+      document.getElementById("available-balance").innerText
+    );
+
+    if (accountNumber.length !== 11) {
+      alert("Please enter a valid account number!");
+      return;
+    }
+
+    if (pin !== validPin) {
+      alert("Please enter a valid pin number!");
+      return;
+    }
+
+    const totalNewBalance = availableBalance + amount;
+
+    document.getElementById("available-balance").innerText = totalNewBalance;
+  });
