5782d14390e190831988d94ca046db08f229753b
rakibhassannayem <rakibhassannayem@gmail.com>
2025-08-24 10:26:13 +0600

    transaction history done

--- home.js
+++ home.js
@@ -1,4 +1,5 @@
 const validPin = 1234;
+const transactionData = [];
 // add money features
 document
   .getElementById("add-money-btn")
@@ -21,6 +22,12 @@ document
 
     const totalNewBalance = availableBalance + amount;
     setInnerText(totalNewBalance);
+
+    const data = {
+      name: "Add Money",
+      date: new Date().toLocaleTimeString(),
+    };
+    transactionData.push(data);
   });
 
 // cashout features
@@ -43,6 +50,12 @@ document.getElementById("withdraw-btn").addEventListener("click", function (e) {
 
   const totalNewBalance = availableBalance - amount;
   setInnerText(totalNewBalance);
+
+  const data = {
+    name: "Cash Out",
+    date: new Date().toLocaleTimeString(),
+  };
+  transactionData.push(data);
 });
 
 // transfer features
@@ -65,6 +78,12 @@ document.getElementById("transfer-btn").addEventListener("click", function (e) {
 
   const totalNewBalance = availableBalance - amount;
   setInnerText(totalNewBalance);
+
+  const data = {
+    name: "Transfer",
+    date: new Date().toLocaleTimeString(),
+  };
+  transactionData.push(data);
 });
 
 // Bonus features
@@ -77,12 +96,74 @@ document.getElementById("bonus-btn").addEventListener("click", function (e) {
   if (coupon.length !== 6) {
     alert("The coupon must be 6 digits");
     return;
-  }else{
-    alert("Congratulations! you've won 500tk.")
+  } else {
+    alert("Congratulations! you've won 500tk.");
   }
 
   const totalNewBalance = availableBalance + 500;
   setInnerText(totalNewBalance);
+
+  const data = {
+    name: "Bonus",
+    date: new Date().toLocaleTimeString(),
+  };
+  transactionData.push(data);
+});
+
+// pay-bill features
+document.getElementById("pay-bill-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const billerAccountNumber = getInputValue("biller-account-number");
+  const amount = getInputValueNumber("pay-bill-amount");
+  const pin = getInputValueNumber("pay-bill-pin");
+  const availableBalance = getInnerText("available-balance");
+
+  if (billerAccountNumber.length !== 11) {
+    alert("Please enter a valid account number!");
+    return;
+  }
+  if (pin !== validPin) {
+    alert("Please enter a valid pin number!");
+    return;
+  }
+
+  const totalNewBalance = availableBalance - amount;
+  setInnerText(totalNewBalance);
+
+  const data = {
+    name: "Pay Bill",
+    date: new Date().toLocaleTimeString(),
+  };
+  transactionData.push(data);
+});
+
+// transaction features
+document.getElementById("transaction").addEventListener("click", function (e) {
+  e.preventDefault();
+  const transactionContainer = document.getElementById("transaction-container");
+  
+  transactionContainer.innerText = "";
+
+  for (const data of transactionData) {
+    const div = document.createElement("div");
+    div.innerHTML = `
+    <div class="border-2 border-[#0808081A] p-3 bg-white flex items-center justify-between rounded-xl mb-2">
+          <div class="flex items-center gap-2">
+            <div class="p-3 rounded-full bg-[#0808080D]">
+              <img src="./assets/wallet1.png" alt="" class="mx-auto" />
+            </div>
+            <div class="text-[#080808]/70">
+              <h1 class="text-lg font-semibold">${data.name}</h1>
+              <p class="text-xs">${data.date}</p>
+            </div>
+          </div>
+          <i class="fa-solid fa-ellipsis-vertical cursor-pointer"></i>
+        </div>
+    `;
+
+    transactionContainer.appendChild(div);
+  }
 });
 
 // toggling
@@ -106,7 +187,17 @@ document.getElementById("bonus").addEventListener("click", function () {
   handleButtonToggle("bonus");
 });
 
-// funcitons to get input values
+document.getElementById("pay-bill").addEventListener("click", function () {
+  handleToggle("pay-bill-parent");
+  handleButtonToggle("pay-bill");
+});
+
+document.getElementById("transaction").addEventListener("click", function () {
+  handleToggle("transaction-parent");
+  handleButtonToggle("transaction");
+});
+
+// funciton to get input values
 function getInputValue(id) {
   const inputField = document.getElementById(id);
   const inputFieldValue = inputField.value;
@@ -118,20 +209,20 @@ function getInputValueNumber(id) {
   const inputFieldValueNumber = parseInt(inputFieldValue);
   return inputFieldValueNumber;
 }
-// functions to get innerTexts
+// function to get innerTexts
 function getInnerText(id) {
   const element = document.getElementById(id);
   const elementValue = element.innerText;
   const elementValueNumber = parseInt(elementValue);
   return elementValueNumber;
 }
-// functions to set innerTexts
+// function to set innerTexts
 function setInnerText(value) {
   const newValue = document.getElementById("available-balance");
   newValue.innerText = value;
 }
 
-//function to handle toggle
+// function to handle toggle
 function handleToggle(id) {
   const forms = document.getElementsByClassName("form");
   for (const form of forms) {
